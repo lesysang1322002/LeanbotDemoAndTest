16d72679528927d9cef9bb997107a55fe4499294
lesysang1322002 <128148253+lesysang1322002@users.noreply.github.com>
2024-08-28 13:57:31 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -278,7 +278,16 @@ function handleChangedValue(event) {
             ir5R = compareThreshold(6);     checkArray[10]=ir5R;
             ir7R = compareThreshold(7);     checkArray[11]=ir7R;
 
-            // console.log("ir6L: " + ir6L + " ir4L: " + ir4L + " ir2L: " + ir2L + " ir0L: " + ir0L + " ir1R: " + ir1R + " ir3R: " + ir3R + " ir5R: " + ir5R + " ir7R: " + ir7R);
+            console.log(
+                "Threshold: " + threshold[0].toString().padEnd(5) + threshold[1].toString().padEnd(5) + threshold[2].toString().padEnd(5) + threshold[3].toString().padEnd(5) +
+                threshold[4].toString().padEnd(5) + threshold[5].toString().padEnd(5) + threshold[6].toString().padEnd(5) + threshold[7].toString().padEnd(5) + 
+                "\nRawValue : " + arrString[5].toString().padEnd(5) + arrString[6].toString().padEnd(5) + arrString[7].toString().padEnd(5) + arrString[8].toString().padEnd(5) + 
+                arrString[9].toString().padEnd(5) + arrString[10].toString().padEnd(5) + arrString[11].toString().padEnd(5) + arrString[12].toString().padEnd(5) +
+                "\niRValue  : " + ir6L.toString().padEnd(5) + ir4L.toString().padEnd(5) + ir2L.toString().padEnd(5) + ir0L.toString().padEnd(5) + 
+                ir1R.toString().padEnd(5) + ir3R.toString().padEnd(5) + ir5R.toString().padEnd(5) + ir7R.toString().padEnd(5)
+            );
+            console.log("\n");            
+            
 
             lineState = arrString[4];
 
@@ -406,6 +415,7 @@ function map(value, in_min, in_max, out_min, out_max) {
 function compareThreshold(index) {
     let irValue = parseInt(arrString[index + 5]);
     threshold[index] = Math.min(threshold[index], irValue * 1.8);
+    // console.log("Threshold " + index + ": " + threshold[index]);
     return irValue > threshold[index] ? 1 : 0;
 }
 
